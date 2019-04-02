333e9a0334632357359710303326b7c192d611b8
Raj Pandey <pandeyraj467@gmail.com>
2019-04-02 17:54:22 +0530

    Change : Modified Some FIles

--- rfid-skill-raj/__init__.py
+++ rfid-skill-raj/__init__.py
@@ -1,58 +1,67 @@
 from mycroft import MycroftSkill
 import RPi.GPIO as GPIO
-from .SimpleMFRC522 import SimpleMFRC522
+# from .SimpleMFRC522 import SimpleMFRC522
 from threading import Thread
+from .Read import user
 import time
 
-reader = SimpleMFRC522()
+reader = user()
 RED = 11
 GREEN = 15
-GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-GPIO.setup(RED,GPIO.OUT)
-GPIO.setup(GREEN,GPIO.OUT)
+GPIO.setup(RED, GPIO.OUT)
+GPIO.setup(GREEN, GPIO.OUT)
+
 
 class Rfid(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         self.stopped = False
+        self.status = False
         self.thread = Thread(target=self.read_thread)
         self.thread.daemon = True
         self.thread.start()
-		
+        self.stop = True
+        self.stop1 = False
+        self.read = False
+
     def read_thread(self):
         while not self.stopped:
-            self.id, self.text = reader.read_no_block()
-            if self.text is not None:
-              #  GPIO.output(GREEN,GPIO.HIGH)
-              #  GPIO.output(RED,GPIO.LOW)
-              #  response = {'text': self.text}
-              #  self.speak_dialog("to.hello.say", data=response)
-              #  time.sleep(10)
-              #  GPIO.cleanup()
-                self.confirm()
-            elif self.text is None:
-              #  self.speak_dialog("to.bye.say")
-              #  GPIO.output(RED,GPIO.HIGH)
-              #  GPIO.output(GREEN,GPIO.LOW)
-                self.unconfirm()
+            if reader != "":
+                if self.stop == True:
+                    self.confirm()
+                    self.stop = False
+                # self.id, self.text = reader.read_no_block()
+                # while self.text is not None:
+                #     if self.stop == True:
+                #         self.confirm()
+                #         self.stop = False
+                # if self.text is None:
+                #     if self.stop1 ==false:
+                #         self.unconfirm()
+                #         self.stop1 = True
+
     def confirm(self):
-        GPIO.output(GREEN,GPIO.HIGH)
-        GPIO.output(RED,GPIO.LOW)
+        GPIO.output(GREEN, GPIO.HIGH)
+        GPIO.output(RED, GPIO.LOW)
         response = {'text': self.text}
         self.speak_dialog("to.hello.say", data=response)
-        time.sleep(2)
-        GPIO.cleanup()
-        return
+        time.sleep(5)
+        GPIO.output(GREEN, GPIO.LOW)
+        GPIO.output(RED, GPIO.HIGH)
+        time.sleep(3)
+        GPIO.output(RED, GPIO.LOW)
+        GPIO.output(GREEN, GPIO.LOW)
+
     def unconfirm(self):
-        GPIO.output(RED,GPIO.HIGH)
-        GPIO.output(GREEN,GPIO.LOW)
+        GPIO.output(RED, GPIO.HIGH)
+        GPIO.output(GREEN, GPIO.LOW)
         self.speak_dialog("to.bye.say")
         time.sleep(2)
-        GPIO.cleanup()
-        return
+
     def shutdown(self):
         self.stopped = True
+
 
 def create_skill():
     return Rfid()
--- rfid-skill-raj/read.py
+++ rfid-skill-raj/read.py
@@ -1,13 +1,102 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#
+#    Copyright 2014,2018 Mario Gomez <mario.gomez@teubi.co>
+#
+#    This file is part of MFRC522-Python
+#    MFRC522-Python is a simple Python implementation for
+#    the MFRC522 NFC Card Reader for the Raspberry Pi.
+#
+#    MFRC522-Python is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    MFRC522-Python is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with MFRC522-Python.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 import RPi.GPIO as GPIO
-from SimpleMFRC522 import SimpleMFRC522
+from .MFRC522 import MFRC522
+#import signal
 
-reader =  SimpleMFRC522()
+continue_reading = True
 
-try:
-	print("Place Your Card")
-	id, text = reader.read()
-	print(id)
-	print(text)
 
-finally:
-	GPIO.cleanup()
+def ___init__():
+    continue_read()
+
+
+# Capture SIGINT for cleanup when the script is aborted
+# def end_read(signal, frame):
+#     global continue_reading
+#     print("Ctrl+C captured, ending read.")
+#     continue_reading = False
+#     GPIO.cleanup()
+
+
+# Hook the SIGINT
+#signal.signal(signal.SIGINT, end_read)
+
+# Create an object of the class MFRC522
+MIFAREReader = MFRC522()
+
+
+# Welcome message
+def continue_read():
+    # This loop keeps checking for chips. If one is near it will get the UID and authenticate
+    while continue_reading:
+        print("Card Found")
+        # Scan for cards
+        (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+        # If a card is found
+        if status == MIFAREReader.MI_OK:
+            print("Card detected")
+
+        # Get the UID of the card
+        (status, uid) = MIFAREReader.MFRC522_Anticoll()
+
+        # If we have the UID, continue
+        if status == MIFAREReader.MI_OK:
+
+            # Print UID
+            print("Card read UID: %s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3]))
+            print(type(uid))
+            # This is the default key for authentication
+            key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+
+            # Select the scanned tag
+            MIFAREReader.MFRC522_SelectTag(uid)
+
+            # Authenticate
+            status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+
+            # Check if authenticated
+            if status == MIFAREReader.MI_OK:
+                MIFAREReader.MFRC522_Read(8)
+                MIFAREReader.MFRC522_StopCrypto1()
+            else:
+                print("Authentication error")
+        print("Card UID : " + str(uid))
+        return uid
+
+
+def user():
+    uid = continue_read()
+    print(uid)
+    print("Card : " + str(type(uid)))
+    if uid == [82, 19, 245, 28, 168]:
+        return "Eric"
+    elif uid == [218, 190, 138, 171, 69]:
+        return "Raj"
+    elif uid == [6, 92, 136, 161, 115]:
+        return "James"
+    else:
+        return ""
+
